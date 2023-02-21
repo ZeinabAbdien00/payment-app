299179969dfba569481c73411548c8dfee59f61a
KONO <aleslam12345@gmail.com>
2023-02-21 08:28:18 +0200

    set initial values to views and setUp their initital logic

--- app/src/main/java/com/example/paymentapp/peresentation/more/MoreFragment.kt
+++ app/src/main/java/com/example/paymentapp/peresentation/more/MoreFragment.kt
@@ -4,21 +4,60 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.paymentapp.R
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.paymentapp.databinding.FragmentMoreBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MoreFragment : Fragment() {
 
+    private lateinit var binding: FragmentMoreBinding
+    private val viewModel: MoreViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false)
+        binding = FragmentMoreBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setViews()
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        binding.passwordLinear.setOnClickListener {
+            binding.usePasswordSwitch.isChecked = !binding.usePasswordSwitch.isChecked
+            lifecycleScope.launch {
+                viewModel.dataStore.setUsePassword(binding.usePasswordSwitch.isChecked)
+            }
+        }
+
+        binding.resetPasswordLinear.setOnClickListener {
+            //todo:Mohamed
+            Toast.makeText(requireContext(),"later",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.notificationLinear.setOnClickListener {
+            binding.notificationsSwitch.isChecked = !binding.notificationsSwitch.isChecked
+            lifecycleScope.launch {
+                viewModel.dataStore.setUseNotifications(binding.notificationsSwitch.isChecked)
+            }
+        }
+    }
+
+    private fun setViews() {
+        lifecycleScope.launch(Dispatchers.Main) {
+            binding.apply {
+                usePasswordSwitch.isChecked = viewModel.isUsePassword()
+                notificationsSwitch.isChecked = viewModel.isUseNotifications()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/paymentapp/peresentation/more/MoreViewModel.kt
+++ app/src/main/java/com/example/paymentapp/peresentation/more/MoreViewModel.kt
@@ -1,9 +1,10 @@
 package com.example.paymentapp.peresentation.more
 
+import androidx.lifecycle.ViewModel
 import com.example.paymentapp.data.dataStore.DataStoreImpl
 import javax.inject.Inject
 
-class MoreViewModel {
+class MoreViewModel : ViewModel() {
     @Inject
     lateinit var dataStore: DataStoreImpl
 
