ca890b7c5330dce3ac0bc3d6e20d2a19ce6837ca
Bassam <bassamouf@std.mans.edu.eg>
2023-02-28 21:13:12 +0200

    add recyclerview

--- app/src/main/java/com/example/paymentapp/peresentation/details/DetailsFragment.kt
+++ app/src/main/java/com/example/paymentapp/peresentation/details/DetailsFragment.kt
@@ -23,5 +23,7 @@ class DetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
     }
 }
