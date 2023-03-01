48b999790bad3d7059bd056cdc2143d74508a7b6
MohamedEmad <aleslam12345@gmail.com>
2023-03-01 06:20:30 +0200

    add history list logic

--- app/src/main/java/com/example/paymentapp/peresentation/details/DetailsFragment.kt
+++ app/src/main/java/com/example/paymentapp/peresentation/details/DetailsFragment.kt
@@ -6,20 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.paymentapp.data.models.BaseModel
 import com.example.paymentapp.databinding.FragmentDetailsBinding
+import com.example.paymentapp.peresentation.recyclerView.HistoryAdapter
 
 class DetailsFragment : Fragment() {
 
     private val args: DetailsFragmentArgs by navArgs()
     private lateinit var binding: FragmentDetailsBinding
     private lateinit var model: BaseModel
+    private lateinit var adapter: HistoryAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         binding = FragmentDetailsBinding.inflate(layoutInflater)
         return binding.root
     }
@@ -27,7 +29,17 @@ class DetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setViews()
+        setupHistoryRV()
+    }
 
+    private fun setupHistoryRV() {
+        try {
+            val myList = model.historyList.split(",") as ArrayList
+            adapter = HistoryAdapter(myList)
+            binding.rvHistoryView.adapter = adapter
+            binding.rvHistoryView.layoutManager = LinearLayoutManager(requireActivity())
+        } catch (_: Exception) {
+        }
     }
 
     private fun setViews() {
@@ -49,9 +61,11 @@ class DetailsFragment : Fragment() {
             //عدد الاقساط المدفوعه
             paidInstallmentEditText.setText(model.numberOfPaidInstallments.toString())
             //قيمة الاقساط المتبقيه
-            costRemainingInstallmentEditText.setText((
-                    model.priceAfterAddition.toFloat() - model.valueOfPayInstallments.toFloat()
-                    ).toString())
+            costRemainingInstallmentEditText.setText(
+                (
+                        model.priceAfterAddition.toFloat() - model.valueOfPayInstallments.toFloat()
+                        ).toString()
+            )
             //عدد اللاقساط المتبقيه
             remainingInstallmentEditText.setText(model.numberOfComingInstallments.toString())
             //يوم السداد
