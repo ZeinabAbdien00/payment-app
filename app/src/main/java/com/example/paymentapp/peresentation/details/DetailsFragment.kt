6491c8affba9259608e59b977bffe77e1c11f586
MohamedEmad <aleslam12345@gmail.com>
2023-03-05 13:36:22 +0200

    fix unclickable attributes

--- app/src/main/java/com/example/paymentapp/peresentation/details/DetailsFragment.kt
+++ app/src/main/java/com/example/paymentapp/peresentation/details/DetailsFragment.kt
@@ -1,21 +1,17 @@
 package com.example.paymentapp.peresentation.details
 
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
-import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.paymentapp.R
 import com.example.paymentapp.data.models.BaseModel
 import com.example.paymentapp.databinding.FragmentDetailsBinding
 import com.example.paymentapp.peresentation.recyclerView.HistoryAdapter
@@ -338,9 +334,7 @@ class DetailsFragment : Fragment() {
         }
 
         binding.backArrow.setOnClickListener {
-
             findNavController().navigateUp()
-
         }
     }
 
